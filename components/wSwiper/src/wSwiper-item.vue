ba8a156f8a38285d84ce85e8770a7bf3e8fbb1ac
Your Name <364049242@qq.com>
2020-03-28 18:46:41 +0800

    修改swiper组件

--- components/wSwiper/src/wSwiper-item.vue
+++ components/wSwiper/src/wSwiper-item.vue
@@ -1,38 +1,122 @@
 <template>
   <div class="swiper-item">
-      <img src="../../../public/1.jpg"/>
+    <ul class="swiper-ul">
+      <li v-for="(value,index) in pre" :key="index" :class="index==0?'leave':'enter'">
+        <img :src="value" />
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name:"wSwiperItem",
-  components:{},
-  props:{
-    imglist:{
-      type:Array
+  name: "wSwiperItem",
+  components: {},
+  props: {
+    imglist: {
+      type: Array
     }
   },
-  data(){
+  data() {
     return {
+      imgArr: this.imglist,
+      listIndex: 0, //当前的索引
+      pre: [], // 向上遍历的数组
+      next: [], // 向下遍历的数组
+      count: 0,
+      start: false
+    };
+  },
+  watch: {},
+  computed: {},
+  methods: {
+    changeIndex(index) {
+      console.log(index, this.listIndex);
+      this.getPre(index);
+      this.listIndex = index;
+      this.start = false
+    },
+    getPre(index) {
+      if (index < 0) {
+        index = this.imgArr.length - 1;
+      } else if (index > this.imgArr.length - 1) {
+        index = 0;
+      }
+      this.pre = [this.imgArr[this.listIndex],this.imgArr[index]];
+      console.log("翻页", this.pre);
+     
+    },
+    getNext(index) {
+      this.next = [this.imgArr[this.listIndex], this.imgArr[index]];
+      console.log("向上翻页", this.next);
     }
   },
-  watch:{},
-  computed:{},
-  methods:{},
-  created(){},
-  mounted(){}
-}
+  created() {},
+  mounted() {
+    this.pre = this.imgArr.slice(0, 2);
+    console.log(this.imgArr, this.pre);
+  }
+};
 </script>
 <style lang="stylus" scoped>
 .swiper-item {
   width: 100%;
   height: 100%;
   z-index: 9999;
- 
+  display: flex;
 }
-.swiper-item img {
+
+.swiper-ul {
   width: 100%;
-  height:inherit;
+  height: 100%;
+  position: relative;
+
+  li {
+    position: absolute;
+    height: inherit;
+    width: 100%;
+
+    & img {
+      width: 100%;
+      height: inherit;
+    }
+  }
+}
+
+.swiper-ul li:nth-child(1) {
+  z-index: 99;
+}
+
+// .swiper-ul li:not(:first-child) {
+// right: -65%;
+// }
+.leave {
+  animation: leave 2s infinite;
+}
+
+@keyframes leave {
+  to {
+    transform: translateX(-100%);
+  }
+
+  from {
+    transform: translateX(10%);
+  }
+}
+
+.enter {
+  animation: enter 3s infinite;
+  animation-delay 1s
+  right: 0px;
+}
+
+@keyframes enter {
+  to {
+    transform: translateX(10%);
+  }
+
+  from {
+    transform: translateX(100%);
+  }
 }
 </style>
--- components/wSwiper/src/wSwiper.vue
+++ components/wSwiper/src/wSwiper.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="wSwiper">
-    <wSwiperItem  :imglist="imglist"></wSwiperItem>
+    <wSwiperItem :imglist="imglist" :listIndex="listIndex" ref="wSwiperItem"></wSwiperItem>
     <div class="dot">
       <ul>
-        <li></li>
+        <li
+          v-for="(value,index) in imglist"
+          :key="index"
+          :class="index ==listIndex?'curli':''"
+          @click="changeIndex(index)"
+        ></li>
       </ul>
     </div>
   </div>
@@ -11,43 +16,62 @@
 
 <script>
 export default {
-  name:"wSwiper",
-  components:{},
-  props:{},
-  data(){
+  name: "wSwiper",
+  components: {},
+  provide: {},
+  props: {},
+  data() {
     return {
-      imglist:[]
+      imglist: ["/1.jpg", "/2.jpg", "/1.jpg", "/2.jpg"],
+      listIndex: 0
+    };
+  },
+  watch: {},
+  computed: {},
+  methods: {
+    changeIndex(index) {
+      this.listIndex = index;
     }
   },
-  watch:{},
-  computed:{},
-  methods:{},
-  created(){},
-  mounted(){}
-}
+  created() {},
+  mounted() {
+    setInterval(() => {
+      if (this.listIndex > this.imglist.length) {
+        this.listIndex = 0;
+      }
+      this.$refs.wSwiperItem.changeIndex(this.listIndex++);
+    }, 10000);
+  }
+};
 </script>
 <style lang="stylus" scoped>
-.wSwiper{
-  height 40vh
-  width 100%
-  position relative
+.wSwiper {
+  height: 40vh;
+  width: 100%;
+  border: 1px solid gray;
+  position: relative;
+  overflow: hidden;
+
   & .dot {
-    position absolute;
-    buttom 6%
-    left 50%
-    transform translate(-50%,-50%)
-    li{
-      width 8px;
-      height 8px;
-      border-radius:50%;
-      background-color gray;
-      cursor pointer
+    position: absolute;
+    left: 50%;
+    bottom: 6%;
+    z-index: 999;
+    transform: translate(-50%, -50%);
+
+    li {
+      display: inline-block;
+      margin-left: 4px;
+      width: 8px;
+      height: 8px;
+      border-radius: 50%;
+      background-color: #b8e9ea;
+      cursor: pointer;
     }
   }
 }
 
-.curli{
-  background-color red
-  }
-
+.curli {
+  background-color: #1ad0b3 !important;
+}
 </style>
